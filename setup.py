3cb59ba96c8c61d5a49d7bc0002cee07fca4bd58
Siddhant Dutta <113273376+elucidator8918@users.noreply.github.com>
2023-06-05 12:06:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup
 
 setup(
     name='weather',
-    version='1.1',
+    version='0.1',
     author="Siddhant Dutta",
+    description='A Python package in a form of a CLI for retrieving weather information with ease',
+    license="MIT",
     author_email="forsomethingnewsid@gmail.com",
+    url='https://github.com/elucidator8918/CoPilot_Hackathon_Weather_CLI',
     py_modules=['main'],
+    python_requires='>=3.6',
     install_requires=['Click', 'Sphinx-Click', 'sphinx-autodoc-annotation',
                       'pytz', 'requests','getpass4'],
     entry_points={
